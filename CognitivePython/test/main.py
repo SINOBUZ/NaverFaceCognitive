9b9df670c907652edb16bbc83cefc88053b97b7c
서준원 <vegarian@overcast.kr>
2019-04-24 02:06:46 +0900

    Update main.py

--- CognitivePython/test/main.py
+++ CognitivePython/test/main.py
@@ -4,11 +4,11 @@ import requests
 client_id = "6GeC8cV4tsgfm3BvrI6Q"
 client_secret = "jKbayzSuQ9"
 url = "https://openapi.naver.com/v1/vision/face"
-# url = "https://openapi.naver.com/v1/vision/celebrity" // 유명인 얼굴인식
 files = {'image': open('go3.png', 'rb')}
 headers = {'X-Naver-Client-Id': client_id, 'X-Naver-Client-Secret': client_secret }
 response = requests.post(url,  files=files, headers=headers)
 rescode = response.status_code
+
 if(rescode==200):
     print (response.text)
 else:
